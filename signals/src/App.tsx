125ae73854aa97a453cd08b8bec7c5c7653c61a4
Theo Richart <theor@unity3d.com>
2023-09-17 12:11:31 -0400

    cleanup

--- signals/src/App.tsx
+++ signals/src/App.tsx
@@ -1,7 +1,6 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+
 import {
   get,
   getDatabase,
@@ -12,12 +11,23 @@ import {
   set,
   update,
 } from "firebase/database";
-import { deleteApp, getApp } from "firebase/app";
+import { deleteApp, getApp, initializeApp } from "firebase/app";
 import {
+  browserSessionPersistence,
   getAuth,
-  createUserWithEmailAndPassword,
   signInAnonymously,
+  User,
 } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAite-peeC9kkgfs_KgcbbLZ-G4K5Aa-VU",
+  authDomain: "signals-10b17.firebaseapp.com",
+  databaseURL: "https://signals-10b17-default-rtdb.firebaseio.com",
+  projectId: "signals-10b17",
+  storageBucket: "signals-10b17.appspot.com",
+  messagingSenderId: "715260614346",
+  appId: "1:715260614346:web:63433ff1bdcb1099edc633",
+};
 
 const configuration = {
   iceServers: [
@@ -27,14 +37,13 @@ const configuration = {
   ],
   iceCandidatePoolSize: 10,
 };
+
 function registerPeerConnectionListeners(
   conn: RTCPeerConnection,
   setState: (s: RTCPeerConnectionState) => void
 ) {
   conn.addEventListener("icegatheringstatechange", () => {
-    console.log(
-      `ICE gathering state changed: ${conn.iceGatheringState}`
-    );
+    console.log(`ICE gathering state changed: ${conn.iceGatheringState}`);
   });
 
   conn.addEventListener("connectionstatechange", () => {
@@ -47,9 +56,7 @@ function registerPeerConnectionListeners(
   });
 
   conn.addEventListener("iceconnectionstatechange ", () => {
-    console.log(
-      `ICE connection state change: ${conn.iceConnectionState}`
-    );
+    console.log(`ICE connection state change: ${conn.iceConnectionState}`);
   });
 }
 const roomPath = "rooms/-NeTabnqkcYVitHHTaj8";
@@ -61,79 +68,82 @@ let peerConnection: RTCPeerConnection;
 
 // }
 
-function App() {
-  const [state, setState] = useState<RTCPeerConnectionState>("closed");
-  const [channel, setChannel] = useState<RTCDataChannel>();
-  const create = async (conn: RTCPeerConnection) => {
-    const db = getDatabase();
-    const roomRef = ref(db, roomPath);
-    await set(roomRef, { name: "asd", users: [] });
+const create = async (conn: RTCPeerConnection) => {
+  const db = getDatabase();
+  const roomRef = ref(db, roomPath);
+  await set(roomRef, { name: "asd", users: [] });
 
-    let sendChannel = conn.createDataChannel("sendChannel");
-    let handleSendChannelStatusChange = (x: any) => {
-      console.log("sendchannel change", x);
-    };
-    sendChannel.onopen = handleSendChannelStatusChange;
-    sendChannel.onclose = handleSendChannelStatusChange;
-    setChannel(sendChannel);
-    conn.addEventListener("icecandidate", (event) => {
-      if (!event.candidate) {
-        console.log("Got final candidate!");
-        return;
-      }
-      console.log("Got candidate: ", event.candidate);
-      set(
-        push(ref(db, roomPath + "/callerCandidates")),
-        event.candidate.toJSON()
-      );
-    });
-
-    const offer = await conn.createOffer();
-    await conn.setLocalDescription(offer);
-    console.log("Created offer:", offer);
-
-    const roomWithOffer = {
-      offer: {
-        type: offer.type,
-        sdp: offer.sdp,
-      },
-    };
-    await update(roomRef, roomWithOffer);
-
-    conn.addEventListener("track", (event) => {
-      console.log("Got remote track:", event.streams[0]);
-      event.streams[0].getTracks().forEach((track) => {
-        console.log("Add a track to the remoteStream:", track);
-        // remoteStream.addTrack(track);
-      });
-    });
-    onValue(ref(db, roomPath + "/answer"), async (snapshot) => {
-      const data = snapshot.val();
-      console.log("on value", data);
-      if (!conn.currentRemoteDescription && data) {
-        console.log("Got remote description: ", data);
-        const rtcSessionDescription = new RTCSessionDescription(data);
-        await conn.setRemoteDescription(rtcSessionDescription);
-      }
-    });
-
-    onChildAdded(ref(db, roomPath + "/calleeCandidates"), async (snapshot) => {
-      const data = snapshot.val();
-      console.log("callee candidate added", data);
-    });
+  let sendChannel = conn.createDataChannel("sendChannel");
+  let handleSendChannelStatusChange = (x: any) => {
+    console.log("sendchannel change", x);
   };
-
-  const join = async (conn: RTCPeerConnection) => {
-    const db = getDatabase();
-    const roomRef = ref(db, roomPath);
-    const room = await get(ref(db, "rooms/-NeTabnqkcYVitHHTaj8"));
-    if (!room.exists()) {
-      console.error("room doesnt exist");
+  sendChannel.onopen = handleSendChannelStatusChange;
+  sendChannel.onclose = handleSendChannelStatusChange;
+  conn.addEventListener("icecandidate", (event) => {
+    if (!event.candidate) {
+      console.log("Got final candidate!");
+      return;
     }
+    console.log("Got candidate: ", event.candidate);
+    set(
+      push(ref(db, roomPath + "/callerCandidates")),
+      event.candidate.toJSON()
+    );
+  });
 
+  const offer = await conn.createOffer();
+  await conn.setLocalDescription(offer);
+  console.log("Created offer:", offer);
+
+  const roomWithOffer = {
+    offer: {
+      type: offer.type,
+      sdp: offer.sdp,
+    },
+  };
+  await update(roomRef, roomWithOffer);
+
+  conn.addEventListener("track", (event) => {
+    console.log("Got remote track:", event.streams[0]);
+    event.streams[0].getTracks().forEach((track) => {
+      console.log("Add a track to the remoteStream:", track);
+      // remoteStream.addTrack(track);
+    });
+  });
+  onValue(ref(db, roomPath + "/answer"), async (snapshot) => {
+    const data = snapshot.val();
+    console.log("on value", data);
+    if (/* !conn.currentRemoteDescription && */ data) {
+      console.log("Got remote description: ", data);
+      const rtcSessionDescription = new RTCSessionDescription(data);
+      if (conn.currentRemoteDescription) {
+        console.warn("clear remote desc");
+        conn.restartIce();
+      }
+      await conn.setRemoteDescription(rtcSessionDescription);
+    }
+  });
+
+  onChildAdded(ref(db, roomPath + "/calleeCandidates"), async (snapshot) => {
+    const data = snapshot.val();
+    console.log("callee candidate added", data);
+  });
+  return sendChannel;
+};
+
+const join = async (conn: RTCPeerConnection) => {
+  const db = getDatabase();
+  const roomRef = ref(db, roomPath);
+  const room = await get(ref(db, "rooms/-NeTabnqkcYVitHHTaj8"));
+  if (!room.exists()) {
+    console.error("room doesnt exist");
+    return Promise.reject("room doesnt exist");
+  }
+
+  return new Promise<RTCDataChannel>(async (resolve, reject) => {
     conn.ondatachannel = (e) => {
       console.log("on datachannel", e);
-      setChannel(e.channel);
+      resolve(e.channel);
     };
     conn.addEventListener("icecandidate", (event) => {
       if (!event.candidate) {
@@ -175,9 +185,23 @@ function App() {
       console.log("on value", data);
       await conn.addIceCandidate(new RTCIceCandidate(data));
     });
-  };
+  });
+};
+
+function App() {
+  const [user, setUser] = useState<User | undefined>();
+  const [state, setState] = useState<RTCPeerConnectionState>("closed");
+  const [channel, setChannel] = useState<RTCDataChannel>();
 
   useEffect(() => {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    getDatabase(app);
+    getAuth(app).setPersistence(browserSessionPersistence);
+
+    signInAnonymously(getAuth()).then((u) => setUser(u.user));
+
     peerConnection = new RTCPeerConnection(configuration);
     registerPeerConnectionListeners(peerConnection, setState);
 
@@ -190,19 +214,24 @@ function App() {
     };
   }, []);
   useEffect(() => {
-    if(channel)
-    channel.onmessage = e => console.log("message", e);
+    if (channel) channel.onmessage = (e) => console.log("message", e);
   }, [channel]);
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>{user?.uid?.substring(0, 7)}</h1>
       <div className="card">
-        <button onClick={() => create(peerConnection)}>Create</button>
+        <button onClick={() => create(peerConnection).then(setChannel)}>
+          Create
+        </button>
 
-        <button onClick={() => join(peerConnection)}>Join</button>
+        <button onClick={() => join(peerConnection).then(setChannel)}>
+          Join
+        </button>
         <span>{state}</span>
-        {state === "connected" && <button onClick={() => channel?.send("test")}>send</button>}
+        {state === "connected" && (
+          <button onClick={() => channel?.send("test")}>send</button>
+        )}
       </div>
     </>
   );
--- signals/src/main.tsx
+++ signals/src/main.tsx
@@ -1,29 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAite-peeC9kkgfs_KgcbbLZ-G4K5Aa-VU",
-  authDomain: "signals-10b17.firebaseapp.com",
-  databaseURL: "https://signals-10b17-default-rtdb.firebaseio.com",
-  projectId: "signals-10b17",
-  storageBucket: "signals-10b17.appspot.com",
-  messagingSenderId: "715260614346",
-  appId: "1:715260614346:web:63433ff1bdcb1099edc633"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-getDatabase(app);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
